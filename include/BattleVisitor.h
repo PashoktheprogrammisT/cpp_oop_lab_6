c97b6526e9e28cab14e74dc1c725a2146ee2829f
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:39:41 +0300

    Update BattleVisitor.h

--- include/BattleVisitor.h
+++ include/BattleVisitor.h
@@ -1,1 +1,9 @@
+#pragma once
+#include <memory>
+#include "NPC.h"
+#include "BattleObserver.h"
 
+class BattleVisitor {
+public:
+    void visit(std::shared_ptr<NPC> npc, std::shared_ptr<NPC> other, std::shared_ptr<BattleObserver> observer);
+};
