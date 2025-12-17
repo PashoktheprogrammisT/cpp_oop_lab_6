636070c7a1279c1912621cae2f6fcb38b9b96b76
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 23:32:30 +0300

    Update BattleObserver.h

--- include/BattleObserver.h
+++ include/BattleObserver.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <vector>
 
 class BattleObserver {
 public:
@@ -18,4 +19,18 @@ public:
 class ConsoleObserver : public BattleObserver {
 public:
     void onKill(const std::string& killer, const std::string& victim) override;
+};
+
+class CompositeObserver : public BattleObserver {
+    std::vector<std::shared_ptr<BattleObserver>> observers;
+public:
+    void addObserver(std::shared_ptr<BattleObserver> observer) {
+        observers.push_back(observer);
+    }
+    
+    void onKill(const std::string& killer, const std::string& victim) override {
+        for (auto& observer : observers) {
+            observer->onKill(killer, victim);
+        }
+    }
 };
