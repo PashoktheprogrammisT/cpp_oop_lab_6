7802c37ac17b981b6df66cc3af743807f56944c2
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 23:36:19 +0300

    Update Dungeon.cpp

--- src/Dungeon.cpp
+++ src/Dungeon.cpp
@@ -1,1 +1,57 @@
+#include "Dungeon.h"
+#include "NPCFactory.h"
+#include "BattleVisitor.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <memory>
 
+void Dungeon::addNPC(std::shared_ptr<NPC> npc) {
+    npcs.push_back(npc);
+}
+
+void Dungeon::printAll() const {
+    for (const auto& npc : npcs) {
+        std::cout << npc->getType() << " " << npc->getName() 
+                  << " at (" << npc->getX() << ", " << npc->getY() << ")" << std::endl;
+    }
+}
+
+void Dungeon::saveToFile(const std::string& filename) const {
+    std::ofstream file(filename);
+    for (const auto& npc : npcs) {
+        file << NPCFactory::saveToString(*npc) << std::endl;
+    }
+    file.close();
+}
+
+void Dungeon::loadFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line)) {
+        auto npc = NPCFactory::loadFromString(line);
+        if (npc) npcs.push_back(npc);
+    }
+    file.close();
+}
+
+void Dungeon::battle(double range, std::shared_ptr<BattleObserver> observer) {
+    BattleVisitor visitor;
+    std::vector<std::shared_ptr<NPC>> deadNPCs;
+    
+    for (size_t i = 0; i < npcs.size(); ++i) {
+        for (size_t j = i + 1; j < npcs.size(); ++j) {
+            if (npcs[i] && npcs[j] && npcs[i]->distanceTo(*npcs[j]) <= range) {
+                npcs[i]->accept(visitor, npcs[j], observer);
+            }
+        }
+    }
+    
+    for (auto& dead : deadNPCs) {
+        auto it = std::find(npcs.begin(), npcs.end(), dead);
+        if (it != npcs.end()) {
+            npcs.erase(it);
+        }
+    }
+}
