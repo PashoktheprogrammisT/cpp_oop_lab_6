95fc915fdf5de8cd690f30ada9cfdccbd9d53c23
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:37:06 +0300

    Update BattleObserver.h

--- include/BattleObserver.h
+++ include/BattleObserver.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <string>
+#include <memory>
 
+class BattleObserver {
+public:
+    virtual ~BattleObserver() = default;
+    virtual void onKill(const std::string& killer, const std::string& victim) = 0;
+};
+
+class FileObserver : public BattleObserver {
+    std::string filename;
+public:
+    FileObserver(const std::string& filename);
+    void onKill(const std::string& killer, const std::string& victim) override;
+};
+
+class ConsoleObserver : public BattleObserver {
+public:
+    void onKill(const std::string& killer, const std::string& victim) override;
+};
