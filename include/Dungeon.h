ecf3d86fed62d07a49485132dfcf19c5d41ef6ec
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:40:26 +0300

    Update Dungeon.h

--- include/Dungeon.h
+++ include/Dungeon.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "NPC.h"
+#include "BattleObserver.h"
 
+class Dungeon {
+    std::vector<std::shared_ptr<NPC>> npcs;
+public:
+    void addNPC(std::shared_ptr<NPC> npc);
+    void printAll() const;
+    void saveToFile(const std::string& filename) const;
+    void loadFromFile(const std::string& filename);
+    void battle(double range, std::shared_ptr<BattleObserver> observer);
+};
