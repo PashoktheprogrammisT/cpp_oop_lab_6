ab132b5f8e6e6efd4c469f20905021fc2e4d74a6
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:47:43 +0300

    Update BattleVisitor.cpp

--- src/BattleVisitor.cpp
+++ src/BattleVisitor.cpp
@@ -1,1 +1,12 @@
+#include "BattleVisitor.h"
+#include "NPC.h"
+#include "BattleObserver.h"
+#include <iostream>
 
+void BattleVisitor::visit(std::shared_ptr<NPC> npc, std::shared_ptr<NPC> other, std::shared_ptr<BattleObserver> observer) {
+    if (npc->canAttack(*other)) {
+        observer->onKill(npc->getName(), other->getName());
+    } else if (other->canAttack(*npc)) {
+        observer->onKill(other->getName(), npc->getName());
+    }
+}
