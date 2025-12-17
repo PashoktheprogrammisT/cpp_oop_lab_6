7bfbe6b09650f1beeda29e0b8c6d23d8375a41df
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:42:59 +0300

    Update NPC.h

--- include/NPC.h
+++ include/NPC.h
@@ -1,1 +1,24 @@
+#pragma once
+#include <string>
+#include <memory>
 
+class BattleVisitor;
+class BattleObserver;
+
+class NPC {
+protected:
+    std::string name;
+    int x, y;
+public:
+    NPC(const std::string& name, int x, int y);
+    virtual ~NPC() = default;
+
+    virtual std::string getType() const = 0;
+    std::string getName() const;
+    int getX() const;
+    int getY() const;
+    double distanceTo(const NPC& other) const;
+
+    virtual bool canAttack(const NPC& other) const = 0;
+    virtual void accept(BattleVisitor& visitor, std::shared_ptr<NPC> attacker, std::shared_ptr<BattleObserver> observer) = 0;
+};
