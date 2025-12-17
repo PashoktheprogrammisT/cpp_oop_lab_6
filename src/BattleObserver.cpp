cf863191168d9cc3aa2c4be96788a9187d769ad0
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:46:25 +0300

    Update BattleObserver.cpp

--- src/BattleObserver.cpp
+++ src/BattleObserver.cpp
@@ -1,1 +1,18 @@
+#include "BattleObserver.h"
+#include <iostream>
+#include <fstream>
 
+FileObserver::FileObserver(const std::string& filename) : filename(filename) {
+    std::ofstream file(filename, std::ios::trunc);
+    file.close();
+}
+
+void FileObserver::onKill(const std::string& killer, const std::string& victim) {
+    std::ofstream file(filename, std::ios::app);
+    file << killer << " killed " << victim << std::endl;
+    file.close();
+}
+
+void ConsoleObserver::onKill(const std::string& killer, const std::string& victim) {
+    std::cout << "[Battle] " << killer << " killed " << victim << std::endl;
+}
