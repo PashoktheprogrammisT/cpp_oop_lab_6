1c0d6700520f5f0ab485882c2a58f7bbbf645bdc
bxusha <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-12-17 22:44:00 +0300

    Update NPCFactory.h

--- include/NPCFactory.h
+++ include/NPCFactory.h
@@ -1,1 +1,12 @@
+#pragma once
+#include <memory>
+#include <string>
 
+class NPC;
+
+class NPCFactory {
+public:
+    static std::shared_ptr<NPC> createNPC(const std::string& type, const std::string& name, int x, int y);
+    static std::shared_ptr<NPC> loadFromString(const std::string& data);
+    static std::string saveToString(const NPC& npc);
+};
